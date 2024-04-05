4459b12e8e81cd2eb5d48bdd2742e9e8aed10027
Vitalii Shapovalov <martyit@ukr.net>
2024-04-05 20:52:01 +0200

    click on items

--- taskInteractions.js
+++ taskInteractions.js
@@ -1,20 +1,36 @@
+// -add class which moves item and makes buttons under 
+// -made 
+
 export class TaskInteractions{
-	constructor(allListContainer, completeListContainer){
+	constructor(allListContainer, completeListContainer, cssObj){
 		this.allTasksContainer = allListContainer
 		this.completeTasksContainer = completeListContainer
+		this.cssObj = {
+			item: '.item',
+			active: 'active',
+			...(cssObj && {})
+		}
 		this.start()
 	}
-	allListClick(id){
-		console.log(`this is done id ${id}`)
+	allListClick(id, list){
 		const task = document.getElementById(id)
-		task.style.transform = 'translate(100px, 0)'
-this.start	}
+		const	activeTask = task.classList.contains(this.cssObj.active)
+
+		list.forEach(item => {
+			item.classList.remove(this.cssObj.active)
+		})
+		if (!activeTask) {
+			task.classList.add(this.cssObj.active)
+		}
+	}
 	addAllListListener(){
-		this.allTasksContainer.querySelectorAll('.item').forEach(task => {
+
+		const allTaskList = this.allTasksContainer.querySelectorAll(this.cssObj.item)
+		
+		allTaskList.forEach(task => {
 			const id = task.getAttribute('id')
 			task.style.transition = '0.3s ease-out 0s'
-
-			task.onclick = this.allListClick.bind(this, id)
+			task.onclick = this.allListClick.bind(this, id, allTaskList)
 		})
 	}
 	start(){
